b5d1fee8440de3168498ffe545759d71adb81f4c
joji_w <hama.wata2@gmail.com>
2014-08-27 15:20:34 +0900

    first commit

---
+++ Classes/MainScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MainScene.cpp
+//  ChatNoir
+//
+//  Created by Joji Watanabe on 2014/08/27.
+//
+//
+
+#include "MainScene.h"
---
+++ Classes/MainScene.h
@@ -0,0 +1,14 @@
+//
+//  MainScene.h
+//  ChatNoir
+//
+//  Created by Joji Watanabe on 2014/08/27.
+//
+//
+
+#ifndef __ChatNoir__MainScene__
+#define __ChatNoir__MainScene__
+
+#include <iostream>
+
+#endif /* defined(__ChatNoir__MainScene__) */
